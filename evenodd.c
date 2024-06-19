ca4a319f6b71ed922db71c0acf6260f82ec7ea73
Komal Pawar <152048397+1417K@users.noreply.github.com>
2024-06-19 20:02:09 +0530

    Add files via upload

---
+++ evenodd.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a;
+    printf("Enter any Number:");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("\nThe number is even");
+    }
+    else
+    {
+        printf("\nThe number is odd"); 
+    }
+    
+}
